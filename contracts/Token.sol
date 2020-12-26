b118dc59ab21cb96faeeb69032c0113b8cab5c65
Carla Trujillo Santiago <cartrujillosa@gmail.com>
2020-12-26 17:38:48 +0100

    license line on top of contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,6 @@
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
 import "hardhat/console.sol";
